cac3a1e48ea0b78198503e5f086cccdf32310bad
AmirHossein <amirhossein.ahmadi.ah@gmail.com>
2026-09-19 22:16:49 +0330

    .

--- examples/echo_bot_webhook.py
+++ examples/echo_bot_webhook.py
@@ -1,6 +1,5 @@
 from peyk import Bot, Dispatcher
 
-
 def main() -> None:
     bot = Bot("TOKEN", platform="telegram")
     dp = Dispatcher()
@@ -10,7 +9,6 @@ def main() -> None:
         await message.answer(message.text or "")
 
     dp.run_webhook(bot, base_url="https://example.com", path_prefix="/webhook")
-
 
 if __name__ == "__main__":
     main()
